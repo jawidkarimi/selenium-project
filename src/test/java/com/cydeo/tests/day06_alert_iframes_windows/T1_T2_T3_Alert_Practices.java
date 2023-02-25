963011d4c107a3b99c35700f74fc9930092af5d7
jawidkarimi <104651569+jawidkarimi@users.noreply.github.com>
2023-02-25 09:36:19 -0800

    Alert practice committed

---
+++ src/test/java/com/cydeo/tests/day06_alert_iframes_windows/T1_T2_T3_Alert_Practices.java
@@ -0,0 +1,73 @@
+package com.cydeo.tests.day06_alert_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.sql.Driver;
+import java.time.Duration;
+
+public class T1_T2_T3_Alert_Practices {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        //1. Open browser
+        driver = WebDriverFactory.getDriver("Chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //2. Go to website: https://practice.cydeo.com/javascript_alerts
+        driver.get("https://practice.cydeo.com/javascript_alerts");
+    }
+
+    @Test
+    public void information_alert_test1(){
+
+        //TC #1: Information alert practice
+
+        //3. Click to "Click for JS Alert" button
+        WebElement informationAlerButton = driver.findElement(By.xpath("//button[.='Click for JS Alert']"));
+        informationAlerButton.click();
+
+
+        //4. Click to OK button from the alert
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+
+        //5. Verify "You successfully clicked an alert " text is displayed.
+        WebElement resultText = driver.findElement(By.xpath("//p[@id='result']"));
+
+        //Failure message will only be displayed if assertion fails
+        Assert.assertTrue(resultText.isDisplayed(),"Result text is NOT displayed!");
+
+        String expectedText = "You successfully clicked an alert";
+        String actualText = resultText.getText();
+        Assert.assertEquals(actualText,expectedText,"Actual result text is NOT as expected!");
+    }
+
+    @Test
+    public void confirmation_alert_test2() throws InterruptedException {
+        //TC #2: Confirmation alert practice
+
+        //3. Click to "Click for JS Confirm" button
+        WebElement confirmationAlert = driver.findElement(By.xpath("//button[.='Click for JS Confirm']"));
+        confirmationAlert.click();
+
+        //4. Click to OK button from the alert
+        Alert alert = driver.switchTo().alert();
+        //Thread.sleep(1000);
+        alert.accept();
+
+        //5. Verify "You clicked: Ok" text is displayed.
+        WebElement resultText = driver.findElement(By.xpath("//p[@id='result']"));
+        Assert.assertTrue(resultText.isDisplayed());
+
+
+    }
+}
